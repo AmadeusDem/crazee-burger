152a8c5a930da17e90b43d6b05a0795100e6b6e4
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-08 15:41:45 +0200

    ✨ F02: ERROR > Add link to LoginPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
   return (
     <>
       <h1>ErrorPage</h1>
-      <button>Retourner à la page d'accueil</button>
+      <Link to="/">
+        <button>Retourner à la page d'accueil</button>
+      </Link>
     </>
   );
 }
