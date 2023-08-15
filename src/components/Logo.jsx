e1dfb18f6b685052a682cdd4273df9f5b281c9cc
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-15 21:48:14 +0200

    💄 F03: LOGO > Add style to the logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,12 +1,13 @@
-import logoImg from "../images/logo-orange.png";
+import logoImg from "/images/logo-orange.png";
 import { styled } from "styled-components";
+import { theme } from "../../theme";
 
 export default function Logo() {
   return (
     <LogoStyled className="logo">
-      <h1>CRAZEE</h1>
+      <h1>Crazee</h1>
       <img src={logoImg} alt="" />
-      <h1>BURGER</h1>
+      <h1>Burger</h1>
     </LogoStyled>
   );
 }
@@ -14,4 +15,18 @@ export default function Logo() {
 const LogoStyled = styled.div`
   display: flex;
   align-items: center;
+
+  img {
+    width: 12.5rem;
+    height: 9.375rem;
+    margin: 0 1.28rem;
+  }
+
+  h1 {
+    text-transform: uppercase;
+    text-align: center;
+    font-family: "Amatic SC";
+    font-size: 6.875rem;
+    color: ${theme.colors.primary};
+  }
 `;
