02d9766bc202ef26b1938dddbc93bef92ce5c183
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-20 15:55:51 +0200

    🚚 F04: THEME > Move theme into the src folder and modify imports

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
-import { theme } from "../../../../theme";
+import { theme } from "../../../theme";
 import { IoChevronForward } from "react-icons/io5";
 import { BsPersonCircle } from "react-icons/bs";
 import TextInput from "../../reusable-ui/TextInput";
