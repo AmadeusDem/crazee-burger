b954a958eace4a13852d2c7fee3b481359420064
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-20 15:45:33 +0200

    💄 F04: ORDER > Main > Add styling

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,10 +1,13 @@
 import { styled } from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Main() {
-  return <MainStyled className="main">Main</MainStyled>;
+  return <MainStyled></MainStyled>;
 }
 
 const MainStyled = styled.main`
-  background: blue;
+  background: ${theme.colors.background_white};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
   flex: 1;
 `;
