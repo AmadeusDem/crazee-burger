0501c0760afe88a12a1674beae42e0be76441068
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-21 18:42:54 +0200

    ♻️ F05: ORDER > Main > Change the flex layout to grid layout

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -5,16 +5,18 @@ import Menu from "./Menu";
 export default function Main() {
   return (
     <MainStyled>
+      <div className="basket">Basket</div>
       <Menu />
     </MainStyled>
   );
 }
 
 const MainStyled = styled.main`
-  display: flex;
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
   overflow: hidden;
   background: ${theme.colors.background_white};
   box-shadow: ${theme.shadows.strong};
   border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
-  flex: 1;
 `;
