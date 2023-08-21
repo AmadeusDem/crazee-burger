44b4eb53628a19aeadffaf2108a84a5347c4b0a1
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-21 15:53:26 +0200

    🚧 F05: ORDER > MAIN > Add layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,13 +1,30 @@
 import { styled } from "styled-components";
 import { theme } from "../../../theme";
+import Products from "./Products";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">Basket</div>
+      <div className="products">Products</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.main`
+  display: flex;
+  overflow: hidden;
   background: ${theme.colors.background_white};
   box-shadow: ${theme.shadows.strong};
   border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
   flex: 1;
+
+  .basket {
+    background: blue;
+  }
+
+  .products {
+    background: yellow;
+    flex: 1;
+  }
 `;
