b16d8fdd43b9a0bfc5f932e6bb7c2791de0b3da4
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-21 10:57:43 +0200

    💄 F05: LOGIN > LoginForm > Add the color for the line in the theme and apply it

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -54,7 +54,7 @@ const LoginFormStyled = styled.form`
   }
 
   hr {
-    border: 1.5px solid ${theme.colors.primary};
+    border: 1.5px solid ${theme.colors.loginLine};
     margin-bottom: ${theme.gridUnit * 5}px;
   }
 
