434b0c05c414018094962b6795c6dea660990cf5
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-29 10:25:32 +0200

    💄 F06: NAVBAR > Add Admin toggle button

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -2,10 +2,15 @@ import { styled } from "styled-components";
 import { theme } from "../../../../theme";
 
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
+      <ToggleButton
+        labelIfChecked="Désactiver le mode admin"
+        labelIfUnchecked="Activer le mode admin"
+      />
       <Profile username={username} />
     </NavbarRightSideStyled>
   );
@@ -14,6 +19,7 @@ export default function NavbarRightSide({ username }) {
 const NavbarRightSideStyled = styled.div`
   color: ${theme.colors.greyBlue};
   display: flex;
+  gap: 3.125rem;
   justify-content: center;
   align-items: center;
   padding: 10px 20px;
