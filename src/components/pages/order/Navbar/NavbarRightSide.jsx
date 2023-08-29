7caad765bbd001b310901698d7929f88b1b69191
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-29 11:22:55 +0200

    💄 F06: NAVBAR > Add styling to the toast

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -8,15 +8,15 @@ import "react-toastify/dist/ReactToastify.css";
 export default function NavbarRightSide({ username }) {
   const onToggle = (e) => {
     if (e.target.checked) {
-      toast("🦄 Wow so easy!", {
-        position: "top-right",
+      toast.info("Mode admin activé", {
+        theme: "dark",
+        position: "bottom-right",
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "light",
       });
     }
   };
@@ -41,4 +41,22 @@ const NavbarRightSideStyled = styled.div`
   justify-content: center;
   align-items: center;
   padding: 10px 20px;
+
+  .toaster {
+    max-width: 300px;
+  }
+
+  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
+    background: ${theme.colors.background_dark};
+  }
+
+  .body-toast {
+    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
+      margin-right: 20px;
+      margin-left: 5px;
+    }
+    div {
+      line-height: 1.3em;
+    }
+  }
 `;
