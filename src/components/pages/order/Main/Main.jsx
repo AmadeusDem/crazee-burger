891ea63b9180f3b6a0a10edd7a12f67ab14a470d
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-29 18:09:25 +0200

    💄 F07: MAIN > Add Admin layout with basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,7 +6,10 @@ export default function Main() {
   return (
     <MainStyled>
       {/* <div className="basket">Basket</div> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin-panel"></div>
+      </div>
     </MainStyled>
   );
 }
@@ -14,9 +17,17 @@ export default function Main() {
 const MainStyled = styled.main`
   display: grid;
   grid-template-columns: 1fr;
+  height: calc(95vh - 5.25rem);
+  border: 3px solid green;
 
-  overflow-y: scroll;
   background: ${theme.colors.background_white};
   box-shadow: ${theme.shadows.strong};
   border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
+
+  .menu-and-admin {
+    position: relative;
+    display: grid;
+    border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
+    overflow: hidden;
+  }
 `;
