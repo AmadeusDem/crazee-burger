e961f26b64e09166c5f818b7f70740918ea7e2e8
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-29 11:21:50 +0200

    ✨ F06: NAVBAR > Create basic toast when toggling admin button

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,16 +1,34 @@
 import { styled } from "styled-components";
 import { theme } from "../../../../theme";
-
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function NavbarRightSide({ username }) {
+  const onToggle = (e) => {
+    if (e.target.checked) {
+      toast("🦄 Wow so easy!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
   return (
     <NavbarRightSideStyled>
       <ToggleButton
+        onToggle={onToggle}
         labelIfChecked="Désactiver le mode admin"
         labelIfUnchecked="Activer le mode admin"
       />
+      <ToastContainer />
       <Profile username={username} />
     </NavbarRightSideStyled>
   );
