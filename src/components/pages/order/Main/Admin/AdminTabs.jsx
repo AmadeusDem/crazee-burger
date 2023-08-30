1f5f6c13bb9a5c1d4f1461d0bb3c9f3e3aa1bac1
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-08-30 16:15:27 +0200

    ♻️ F07: ADMIN > AdminTabs > Map over the tabs and manage button selection

--- src/components/pages/order/Main/Admin/AdminTabs.jsx
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -1,14 +1,19 @@
 import { styled } from "styled-components";
 import Tab from "../../../../reusable-ui/Tab";
 import { FiChevronDown, FiChevronUp } from "react-icons/fi";
-import { AiOutlinePlus } from "react-icons/ai";
-import { MdModeEditOutline } from "react-icons/md";
+
 import { useContext } from "react";
 import { AdminContext } from "../../../../../context/AdminContext";
 import { theme } from "../../../../../theme";
+import { tabs } from "./tabsConfig";
 
 export default function AdminTabs() {
-  const { isPanelOpen, setIsPanelOpen } = useContext(AdminContext);
+  const { isPanelOpen, setIsPanelOpen, selectedTab, setSelectedTab } = useContext(AdminContext);
+
+  const selectTab = (tabIndex) => {
+    setIsPanelOpen(true);
+    setSelectedTab(tabIndex);
+  };
 
   return (
     <AdminTabsStyled>
@@ -17,8 +22,15 @@ export default function AdminTabs() {
         onClick={() => setIsPanelOpen(!isPanelOpen)}
         className={!isPanelOpen && "is-active"}
       />
-      <Tab Icon={<AiOutlinePlus />} label="Ajouter un produit" />
-      <Tab Icon={<MdModeEditOutline />} label="Modifier un produit" />
+      {tabs.map((tab) => (
+        <Tab
+          key={tab.index}
+          Icon={<tab.icon />}
+          label={tab.label}
+          className={selectedTab === tab.index && "is-active"}
+          onClick={() => selectTab(tab.index)}
+        />
+      ))}
     </AdminTabsStyled>
   );
 }
