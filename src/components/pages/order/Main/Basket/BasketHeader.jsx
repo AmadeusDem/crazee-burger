a63c211e434b9711e874684370e1be11a2f177aa
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-09-13 13:04:52 +0200

    💄 F10: Basket > Header > Add style

--- src/components/pages/order/Main/Basket/BasketHeader.jsx
+++ src/components/pages/order/Main/Basket/BasketHeader.jsx
@@ -1,9 +1,34 @@
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 export default function BasketHeader() {
-  return <BasketHeaderStyled>BasketHeader</BasketHeaderStyled>;
+  return (
+    <BasketHeaderStyled>
+      <span>Total</span>
+      <span className="euros">0,00 €</span>
+    </BasketHeaderStyled>
+  );
 }
 
 const BasketHeaderStyled = styled.div`
-  background: red;
+  // Position and layout
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  // Box model
+  padding: 0 16px;
+
+  // Background
+  background: ${theme.colors.background_dark};
+
+  // Typography
+  font-family: Amatic SC;
+  font-size: 36px;
+  letter-spacing: 2px;
+  color: ${theme.colors.primary};
+
+  .euros {
+    font-weight: 700;
+  }
 `;
