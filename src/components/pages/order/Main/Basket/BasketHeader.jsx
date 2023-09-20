e82da008699dbf5c02f6f6c7eb803067327ba5bb
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-09-20 17:18:21 +0200

    ♻️ F12: BasketHeader > Change how the total is calculated with new basket configuration

--- src/components/pages/order/Main/Basket/BasketHeader.jsx
+++ src/components/pages/order/Main/Basket/BasketHeader.jsx
@@ -1,12 +1,24 @@
 import styled from "styled-components";
 import { theme } from "../../../../../theme";
 import { formatPrice } from "../../../../../utils/maths";
+import { useContext } from "react";
+import { AdminContext } from "../../../../../context/AdminContext";
+import { find } from "../../../../../utils/array";
 
-export default function BasketHeader({ total = 0 }) {
+export default function BasketHeader() {
+  const { basket, menu } = useContext(AdminContext);
+
+  const basketTotal = basket.reduce((total, product) => {
+    const price = parseFloat(find(product.id, menu).price);
+
+    if (isNaN(price)) return total;
+    return (total += product.quantity * price.toFixed(1));
+  }, 0);
+
   return (
     <BasketHeaderStyled>
       <span>Total</span>
-      <span className="euros">{formatPrice(total.toFixed(2))}</span>
+      <span className="euros">{formatPrice(basketTotal.toFixed(2))}</span>
     </BasketHeaderStyled>
   );
 }
