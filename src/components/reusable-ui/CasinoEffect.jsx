63f2fd94f1f997c3eae9893800e8e9e340510e24
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-09-26 15:23:21 +0200

    💫 F14: Basket > Total > Add CasinoEffect

--- src/components/reusable-ui/CasinoEffect.jsx
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -1,3 +1,46 @@
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import styled from "styled-components";
+
 export default function CasinoEffect({ count, className }) {
-  return <span className={className}>{count}</span>;
+  return (
+    <CasinoEffectStyled>
+      <TransitionGroup component={null}>
+        <CSSTransition key={count} classNames="casino" timeout={300}>
+          <span className={className}>{count}</span>
+        </CSSTransition>
+      </TransitionGroup>
+    </CasinoEffectStyled>
+  );
 }
+
+const CasinoEffectStyled = styled.div`
+  position: relative;
+  /* overflow-y: hidden; */
+  span {
+    display: inline-block;
+  }
+
+  .casino-enter {
+    transform: translateY(100%);
+    opacity: 0;
+  }
+
+  .casino-enter-active {
+    transform: translateY(0%);
+    transition: 300ms;
+    opacity: 1;
+  }
+
+  .casino-exit {
+    transform: translateY(0%);
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+
+  .casino-exit-active {
+    transform: translateY(-100%);
+    transition: 300ms;
+    opacity: 0;
+  }
+`;
