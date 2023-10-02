1660b296ae7e0cf589378b5a14027183107c42d0
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-10-02 12:49:07 +0200

    🚧 F15: Admin > Form > Add the two selects

--- src/components/pages/order/Main/Admin/AdminPanel/admin-forms/Form/Form.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/admin-forms/Form/Form.jsx
@@ -4,6 +4,9 @@ import Input from "../../../../../../../reusable-ui/Input";
 import { getInputConfig } from "../../inputConfig";
 import { forwardRef } from "react";
 import { theme } from "../../../../../../../../theme";
+import BooleanSelect from "../../../../../../../reusable-ui/BooleanSelect";
+import { FiPackage } from "react-icons/fi";
+import { GoMegaphone } from "react-icons/go";
 
 const Form = forwardRef(({ product, onChange, onSubmit, children, onBlur, onFocus }, ref) => {
   const inputs = getInputConfig(product);
@@ -21,10 +24,24 @@ const Form = forwardRef(({ product, onChange, onSubmit, children, onBlur, onFocu
           onFocus={onFocus}
         />
       ))}
-      <select name="isAvailable" className="availability-select">
+      {/* <select name="isAvailable" className="availability-select">
         <option value="true">En stock</option>
         <option value="false">En rupture</option>
-      </select>
+      </select> */}
+      <BooleanSelect
+        className="availability-select"
+        Icon={<FiPackage />}
+        trueText="En stock"
+        falseText="En rupture"
+        name="isAvailable"
+      />
+      <BooleanSelect
+        className="advertising-select"
+        Icon={<GoMegaphone />}
+        trueText="Sans pub"
+        falseText="Avec pub"
+        name="isAdvertised"
+      />
       <div className="submit">{children}</div>
     </FormStyled>
   );
@@ -36,13 +53,13 @@ export default Form;
 const FormStyled = styled.form`
   // Position and layout
   display: grid;
-  grid-gap: ${theme.spacing.xs} ${theme.spacing.md};
+  grid-gap: ${theme.spacing.xs} ${theme.spacing.xs};
   grid-template-columns: 20% 1fr 1fr 1fr;
   grid-template-rows: repeat(4, 1fr);
   grid-template-areas:
     "preview name name name"
     "preview image image image"
-    "preview price availability ."
+    "preview price availability advertising"
     ". submit submit submit";
 
   // Box model (from outside in)
@@ -68,5 +85,9 @@ const FormStyled = styled.form`
 
   .availability-select {
     grid-area: availability;
+  }
+
+  .advertising-select {
+    grid-area: advertising;
   }
 `;
